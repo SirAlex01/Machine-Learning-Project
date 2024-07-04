4db9e14fa25021581661cab5657af084468c6b6f
SirAlex01 <113248340+SirAlex01@users.noreply.github.com>
2024-07-05 01:45:27 +0200

    Update main_qtable.py

--- QTable/main_qtable.py
+++ QTable/main_qtable.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
 import dill
-from QTable import QTableLearning
+from qtable import QTableLearning
 
 # Initialize the Bipedal Walker environment
 env = gym.make('BipedalWalker-v3')#, render_mode="human")
